559db6a8493788d5a454cf00829acf43e331859a
Fola <folaakingbe@yahoo.com>
2018-03-13 15:59:40 -0400

    should be working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
     {
-        name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Fire",
+        photo: "http://www.canyon-news.com/wp-content/uploads/2015/04/fire.jpg",
         scores: [
             5,
             1,
@@ -14,7 +14,87 @@ var friendsArray = [
             4,
             1
         ]
+    },
+    {
+        name: "Ice",
+        photo: "https://d1o50x50snmhul.cloudfront.net/wp-content/uploads/2017/09/08165304/gettyimages-558312385-800x533.jpg",
+        scores: [
+            1,
+            2,
+            3,
+            1,
+            5,
+            1,
+            1,
+            5,
+            3,
+            1
+        ]
+    },
+    {
+        name: "Lightning",
+        photo: "https://us-east-1.tchyn.io/snopes-production/uploads/2015/05/lightning.jpg?resize=865%2C452",
+        scores: [
+            1,
+            1,
+            3,
+            5,
+            1,
+            3,
+            5,
+            1,
+            5,
+            4
+        ]
+    },
+    {
+        name: "Water",
+        photo: "https://hips.htvapps.com/htv-prod-media.s3.amazonaws.com/images/ocean-water-1479055199.jpg?crop=1.00xw:1.00xh;0,0&resize=900:*",
+        scores: [
+            1,
+            1,
+            1,
+            4,
+            2,
+            2,
+            3,
+            3,
+            2,
+            5
+        ]
+    },
+    {
+        name: "Earth",
+        photo: "https://vignette.wikia.nocookie.net/powerlisting/images/3/3e/Dry_land-wallpaper-1366x768.jpg/revision/latest/scale-to-width-down/516?cb=20151111123851",
+        scores: [
+            2,
+            3,
+            3,
+            1,
+            3,
+            4,
+            1,
+            5,
+            4,
+            2
+        ]
+    },
+    {
+        name: "Wind",
+        photo: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OS8xNDkvb3JpZ2luYWwvaHVycmljYW5lLWthdHJpbmEtbmFzYS5qcGc=",
+        scores: [
+            3,
+            4,
+            3,
+            1,
+            5,
+            3,
+            2,
+            3,
+            3,
+            2
+        ]
     }
-]
+];
 
 module.exports = friendsArray;
